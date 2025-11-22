256011ab12f1725a2b7435d6c8a1891d3a31542a
denzang <dpak@ostriz.com>
2025-11-22 06:14:34 +0530

    Added Neon CSS

--- js/hk2.js
+++ js/hk2.js
@@ -18,6 +18,9 @@ document.addEventListener('keydown', (event) => {
         return;
 }
 
+    if (activeElement.className === 'search') {
+        return;
+    }
     if (event.ctrlKey || event.altKey || event.metaKey) {
         return;
     }
